02f956251634b43d5615be3a89f7221fd6c2f369
Alex Alexander <alex.alexander@rackspace.co.uk>
2015-01-22 11:07:55 +0000

    added function: identify origin of mail

--- MailHeader_Inspector.py
+++ MailHeader_Inspector.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import os
+import os, random,subprocess,re
 # Find Mail queue size
 
 #print number of folders in directory
@@ -34,12 +34,6 @@ def queue_size(queue_loc,mta):
 		mail_queue_num = {}
 		for i in m_queue:
 			mail_queue_num[i] = fcount(queue_loc+i)
-		#print mail_queue_num
-		#deferred_mails = fcount(queue_loc+"deferred")
-		#active_mails = fcount(queue_loc+"active")
-		#bounced_mails = fcount(queue_loc+"bounce")
-		#corrupt_mails = fcount(queue_loc+"corrupt")
-		#total_mails = deferred_mails + active_mails + bounced_mails + corrupt_mails
 		print "Total Messages: ", mail_queue_num['bounce']+ mail_queue_num['deferred']+mail_queue_num['active']+mail_queue_num['corrupt']
 		print "Bounced Mail Queue :", mail_queue_num['bounce']
 		print "Deffered Mail Queue :", mail_queue_num['deferred']
@@ -59,15 +53,62 @@ def queue_size(queue_loc,mta):
 		print "MTA running on this server is not supported by this script"
 
 		
-#path = "/var/spool/postfix/test"
-
-		
-#print fcount(path)
-
 queue_size("/var/spool/postfix/","Postfix") 
 
-#print fcount("/var/spool/postfix/test/")
-#topdir = '/var/spool/postfix/test/'
+# Get a specified number mail headers from specififed queue
+def getRandMailHeaders(queue,n):
+	for dirpath, dirnames, files in os.walk(queue):
+		if len(files)>=n:
+                	return random.sample(files,n)
+		else:
+			return files
+
+# Read a mail
+def viewMail(mid,mta):
+	if mta=="Postfix":
+		read_mail = "postcat -q " + mid
+	elif mta=="Qmail":
+		read_mail = "find /var/qmail/queue/mess/ -name" + mid
+        p = subprocess.Popen(read_mail, stdout=subprocess.PIPE, shell=True)
+        output, err = p.communicate()
+        return output
+	
+#print viewMail("3B77413C1B3")
+
+#Get a list of line of lines contains a specific word
+def grepfunc(text,pattern):
+	l = []
+	for i in text.split('\n'):
+                if re.search(pattern,i):
+                        for w in i.split(" "):
+                                l.append(w)
+			l.append('\n')
+        return l
+
+
+# Idetify if the mail was sent via PHP script or from Mail authentication
+def mailOrigin(mid,mta):
+	#mail = viewMail(mid,mta)
+	f = open('/var/spool/postfix/deferred/3/3A77414C1B3','r')
+	mail = f.read()
+	f.close()
+	if mta=="Postfix":
+		if grepfunc(mail,"Received")[-2:-1][0][:-1]==110:	
+			print "Auth User"
+		else:
+			print "PHP Script"
+
+	elif mta=="Qmail":
+		qmail_list=grepfunc(mail,"Received: \(qmail")
+		for i in qmail_list:
+			if re.search("network",i):	
+				print "Auth User"
+			else: 
+				print "PHP Script"
+
+	
+mailOrigin("3B77413C1B3","Postfix")
+
 
 #print folderCount
 # Find mail with lots of receipent
