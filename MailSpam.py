764d3271321b8c69f64b6d62566bc77bde2af6e7
Alex Alexander <alex.alexander@rackspace.co.uk>
2015-01-27 14:42:41 +0000

    possible chnage

--- MailSpam.py
+++ MailSpam.py
@@ -2,7 +2,7 @@
 
 import os,random,subprocess,re,time,datetime,platform
 
-spam_keywords=['Vigara','Viigara' ,'aDult','Debt','already approved', 'already wealthy', 'amazing new discovery', 'amazing pranks', 'an excite game', 'and you save','nasty','babe','fuck']
+spam_keywords=['sex','Vigara','Viigara' ,'aDult','Debt','already approved', 'already wealthy', 'amazing new discovery', 'amazing pranks', 'an excite game', 'and you save','nasty','babe','fuck']
 
 
 # This class discovers all required feature of environement
@@ -88,16 +88,63 @@ class EnvironmentDiscovery:
 		else: 
 			return "Not Supported"
 
-e = EnvironmentDiscovery()
-MTA=e.mta_type()
-print "Mail Service is: ", MTA
-print "Plesk server? ", e.is_plesk()
+#e = EnvironmentDiscovery()
+#MTA=e.mta_type()
+#print "Mail Service is: ", MTA
+#print "Plesk server? ", e.is_plesk()
 #print e.mail_log_path(e.linux_dist()[0],e.is_plesk())
-MAILLOG_PATH=e.mail_log_path(e.linux_dist()[0],e.is_plesk())
-print MAILLOG_PATH
+#MAILLOG_PATH=e.mail_log_path(e.linux_dist()[0],e.is_plesk())
+#print MAILLOG_PATH
 
-MAIL_QUEUE_LOC=e.mail_queue_loc(MTA)
-print MAIL_QUEUE_LOC
+#MAIL_QUEUE_LOC=e.mail_queue_loc(MTA)
+#print MAIL_QUEUE_LOC
+
+
+class MailParser:
+	# Read X number of lines of maillog file from bottom. "tail" like function 
+	def tail(self,f, lines=5000):
+    		total_lines_wanted = lines
+
+    		BLOCK_SIZE = 1024
+    		f.seek(0, 2)
+    		block_end_byte = f.tell()
+    		lines_to_go = total_lines_wanted
+    		block_number = -1
+    		blocks = [] # blocks of size BLOCK_SIZE, in reverse order starting
+                # from the end of the file
+    		while lines_to_go > 0 and block_end_byte > 0:
+        		if (block_end_byte - BLOCK_SIZE > 0):
+            			# read the last block we haven't yet read
+            			f.seek(block_number*BLOCK_SIZE, 2)
+            			blocks.append(f.read(BLOCK_SIZE))
+        		else:
+            			# file too small, start from begining
+            			f.seek(0,0)
+            			# only read what was not read
+            			blocks.append(f.read(block_end_byte))
+        		lines_found = blocks[-1].count('\n')
+        		lines_to_go -= lines_found
+        		block_end_byte -= BLOCK_SIZE
+        		block_number -= 1
+    		all_read_text = ''.join(reversed(blocks))
+	    	return '\n'.join(all_read_text.splitlines()[-total_lines_wanted:])
+
+
+
+	# Get list of email address authenticated with IPs
+	def auth_email_list(self,maillog_loc,pattern,pos1,pos2):
+		fo = open(maillog_loc, "r")
+		file = self.tail(fo)
+		auth_emails=defaultdict(list)
+		for i in file.split('\n'):
+			if re.search(pattern,i):
+				l = []
+                		for w in i.split(" "):
+					l.append(w) 
+				auth_emails[l[pos1]].append(l[pos2])
+        	fo.close()
+		return auth_emails
+
 # Find Mail queue size
 
 #print number of folders in directory
@@ -165,7 +212,7 @@ def queue_size(queue_loc,mta):
 		print "MTA running on this server is not supported by this script"
 
 		
-queue_size(MAIL_QUEUE_LOC,MTA) 
+#queue_size(MAIL_QUEUE_LOC,MTA) 
 
 # Get a specified number mail headers from specififed queue
 def getRandMailHeaders(queue,n):
@@ -320,12 +367,29 @@ def verifySpam(mta):
 			print "Nothing to be done"
 			return False
 
-#for i in range(3):
-#	if verifySpam("Postfix")==True:
-#		break
+
+
+
+e = EnvironmentDiscovery()
+MTA=e.mta_type()
+#print "Mail Service is: ", MTA
+#print "Plesk server? ", e.is_plesk()
+#print e.mail_log_path(e.linux_dist()[0],e.is_plesk())
+MAILLOG_PATH=e.mail_log_path(e.linux_dist()[0],e.is_plesk())
+#print MAILLOG_PATH
+MAIL_QUEUE_LOC=e.mail_queue_loc(MTA)
+#print MAIL_QUEUE_LOC
+
+#m=MailParser()
+#m.auth_email_list(MAILLOG_PATH,)
+
+queue_size(MAIL_QUEUE_LOC,MTA)
+for i in range(3):
+	if verifySpam(MTA)==True:
+		break
 
 #isSpam("/var/spool/postfix/deferred","Postfix")
-verifySpam(MTA)		
+#verifySpam(MTA)		
 #print folderCount
 # Find mail with lots of receipent
 
