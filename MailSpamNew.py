ddd50dc0cc5e9f742f590cc1ca48e10bf4c55528
Alex Alexander <alex.alexander@rackspace.co.uk>
2015-02-13 14:59:00 +0000

    icanhaz

--- MailSpamNew.py
+++ MailSpamNew.py
@@ -613,7 +613,7 @@ def deliverability():
 	rdns=0
 	#myIP=socket.gethostbyname(socket.gethostname())
 	ip_cmd = "curl -4 icanhazip.com"
-	ip_p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+	ip_p = subprocess.Popen(ip_cmd, stdout=subprocess.PIPE, shell=True)
         output_ip, err_ip = ip_p.communicate()
 	myIP=output_ip    
 
